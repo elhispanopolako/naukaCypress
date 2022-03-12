98bb9a3cfd0fe41661d08fa29eb8745e769e304f
elhispanopolako <tadeo98hiszpan@gmail.com>
2022-03-12 20:01:16 +0100

    Rename files

--- cypress/integration/google.spec.js
+++ cypress/integration/google.spec.js
@@ -1,12 +1,18 @@
 /// <reference types="Cypress" />
 
 describe('Cy google się uruchamia', () => {
+    beforeEach(() => {
+        cy.visit('https://www.google.com')
+          cy.get('#L2AGLb').click()
+    })
     it('should open google.com', () => {
-        cy.visit('www.google.com')
-        cy.get('#L2AGLb > .QS5gu').click();
         cy.get('.gLFyf').type('luczniczQA{Enter}');
         cy.get('[href="https://pl-pl.facebook.com/LuczniczQA/"] > .LC20lb')
             .contains('bydgoska społeczność testerska')
-
+    })
+     it("login", () => {
+        cy.get('[name="q"]').type('lol').clear().should('not.contain','lol')
+        cy.get('.lnXdpd').click();
+            
     })
 })
